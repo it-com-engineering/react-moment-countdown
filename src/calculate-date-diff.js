64fc87e19b8cf0b2e295a867250b1c737fb8d02c
Nate Eagle <nate.eagle@it.com>
2017-09-20 16:47:38 -0400

    Replace tabs with spaces

--- src/calculate-date-diff.js
+++ src/calculate-date-diff.js
@@ -8,30 +8,30 @@ export const allKeys = [
 ];
 
 export const keyLabels = {
-	weeks: {
-		singular: 'week',
-		plural: 'weeks'
-	},
-	days: {
-		singular: 'day',
-		plural: 'days'
-	},
-	hrs: {
-		singular: 'hr',
-		plural: 'hrs'
-	},
-	min: {
-		singular: 'min',
-		plural: 'min'
-	},
-	sec: {
-		singular: 'sec',
-		plural: 'sec'
-	},
-	ms: {
-		singular: 'ms',
-		plural: 'ms'
-	}
+  weeks: {
+    singular: 'week',
+    plural: 'weeks'
+  },
+  days: {
+    singular: 'day',
+    plural: 'days'
+  },
+  hrs: {
+    singular: 'hr',
+    plural: 'hrs'
+  },
+  min: {
+    singular: 'min',
+    plural: 'min'
+  },
+  sec: {
+    singular: 'sec',
+    plural: 'sec'
+  },
+  ms: {
+    singular: 'ms',
+    plural: 'ms'
+  }
 };
 
 export const conversions = {
@@ -43,15 +43,25 @@ export const conversions = {
   weeks: 604800000
 };
 
-export default function calculateDateDiff(fromMoment, toMoment, keys = allKeys) {
-  const totalDelta = toMoment.toDate() - fromMoment.toDate();
-  let delta = totalDelta;
-  const remaining = {};
-  keys.forEach(key => {
-    remaining[key] = Math.floor(delta / conversions[key]);
-    delta -= remaining[key] * conversions[key];
-  });
-  remaining.complete = totalDelta <= 0;
+function calculateDateDiff(fromMoment, toMoment) {
+  var keys = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : allKeys;
+
+  var totalDelta = toMoment.toDate() - fromMoment.toDate();
+  var delta = totalDelta;
+  var remaining = {};
+
+  if (totalDelta > 0) {
+    keys.forEach(function (key) {
+      remaining[key] = Math.floor(delta / conversions[key]);
+      delta -= remaining[key] * conversions[key];
+    });
+    remaining.complete = false;
+  } else {
+    keys.forEach(function (key) {
+      remaining[key] = 0;
+    });
+    remaining.complete = true;
+  }
   return remaining;
 }
 
